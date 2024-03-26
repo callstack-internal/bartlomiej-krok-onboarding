cf5429837302104553975c43d6d1179ad92dcf07
bartekkrok <bartek.krok@gmail.com>
2024-03-26 16:42:34 +0100

    Create location native module

---
+++ android/app/src/main/java/com/bkonboarding/LocationModule.kt
@@ -0,0 +1,11 @@
+package com.bkonboarding
+
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.ReactContext
+import com.facebook.react.bridge.ReactContextBaseJavaModule
+import com.facebook.react.bridge.ReactMethod
+
+class LocationModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
+    override fun getName(): String = "LocationModule"
+}
---
+++ src/hooks/location/useLocation.ts
@@ -0,0 +1,9 @@
+export const useLocation = () => {
+  const cords = {
+    latitude: 0,
+    longitude: 0,
+  };
+  return {
+    cords,
+  };
+};
